e9aa7bb9ea48448294f5b3669f9eb4cbded5ab91
luksius <luksiuslukas2@gmail.com>
2020-03-16 11:41:20 +0200

    Updates

---
+++ precache-manifest.d2ccf925c15b74edf591227ff418d0ce.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6713f766f1352ee16c938b1f73847ee1",
+    "url": "/ravenous/index.html"
+  },
+  {
+    "revision": "645f0afb78d8dd518cc4",
+    "url": "/ravenous/static/css/main.519422c9.chunk.css"
+  },
+  {
+    "revision": "99c4d384fc8639990dec",
+    "url": "/ravenous/static/js/2.b03a254c.chunk.js"
+  },
+  {
+    "revision": "5356fa2f66e46e6c05e4cbe319ac7f1d",
+    "url": "/ravenous/static/js/2.b03a254c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "645f0afb78d8dd518cc4",
+    "url": "/ravenous/static/js/main.6acc52fe.chunk.js"
+  },
+  {
+    "revision": "b27c02bdb56a3c933bce",
+    "url": "/ravenous/static/js/runtime-main.71b1131c.js"
+  }
+]);
